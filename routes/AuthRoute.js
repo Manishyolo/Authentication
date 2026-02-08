4f62253fb07f4c5817a7ff5088e507d78c3557a2
Manish kumar <manish04kumarsingh@gmail.com>
2026-02-08 14:20:34 +0530

    feat: implement user registration and login with bcrypt for password hashing

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 
 const AuthRouter = express.Router();
@@ -15,18 +16,53 @@ AuthRouter.post("/register",async (req,res)=>{
             message:"user already exisit"
         })
     }
+  const hashedPassword = await bcrypt.hash(password,12)
+  console.log(hashedPassword);
 
     const user = await userModel.create({
-        name,email,password
+        name,email,password:hashedPassword
     })
-    const token = jwt.sign({name:user.name,email:user.email},process.env.JWT_SECRET)
+    const token = jwt.sign({name:user._id},process.env.JWT_SECRET)
   
-    res.cookie(token)
+    res.cookie("jwt_token",token);
+
     res.status(201).json({
         message:"user Created successfully",
         user:user,
         token:token
     })
+
+})
+
+
+AuthRouter.post("/login",async(req,res)=>{
+     let {email,password} = req.body;
+
+
+      const user = await userModel.findOne({email});
+
+      if(!user){
+        return res.status(404).json({
+            message:"User does not exist please register"
+        })
+      }
+       
+      const isPasswordCorrect = await bcrypt.compare(password,user.password);
+
+      if(!isPasswordCorrect){
+        return res.status(400).json({
+            message:"Invalid password"
+        })
+      }
+         const token = jwt.sign({name:user._id},process.env.JWT_SECRET)
+      
+         res.cookie("jwt_token",token);
+         
+      res.status(200).json({
+        message:"Login succesfully",
+        user:user,
+        token:token
+      })
 
 })
 
